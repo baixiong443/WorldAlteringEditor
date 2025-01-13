bc9b913623ea6323bf394edae056d00915645fa7
Rampastring <rampastring@outlook.com>
2025-01-13 17:03:03 +0200

    Bump version

--- src/TSMapEditor/Constants.cs
+++ src/TSMapEditor/Constants.cs
@@ -5,7 +5,7 @@ namespace TSMapEditor
 {
     public static class Constants
     {
-        public const string ReleaseVersion = "1.3.9";
+        public const string ReleaseVersion = "1.4.0";
 
         public static int CellSizeX = 48;
         public static int CellSizeY = 24;
