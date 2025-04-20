de87e1cd5254d95343aa9f7fc02be50eba5d95ce
Shushishtok <almogosheriko@gmail.com>
2025-04-20 07:24:35 +0300

    Add script for disabling debug triggers

---
+++ src/TSMapEditor/Config/Scripts/Disable All Debug Triggers.cs
@@ -0,0 +1,63 @@
+﻿// Script for disabling all debug triggers in WAE.
+
+// Using clauses.
+// Unless you know what's in the WAE code-base, you want to always include
+// these "standard usings".
+using System;
+using TSMapEditor;
+using TSMapEditor.Models;
+using TSMapEditor.CCEngine;
+using TSMapEditor.Rendering;
+using TSMapEditor.GameMath;
+using TSMapEditor.UI.Windows;
+using Rampastring.XNAUI;
+
+namespace WAEScript
+{
+    public class DisableAllDebugTriggersScript
+    {
+        /// <summary>
+        /// Returns the description of this script.
+        /// All scripts must contain this function.
+        /// </summary>
+        public string GetDescription() => "This script will disable all triggers with 'debug' in their name (case insensitive). Continue?";
+
+        /// <summary>
+        /// Returns the message that is presented to the user if running this script succeeded.
+        /// All scripts must contain this function.
+        /// </summary>
+        public string GetSuccessMessage()
+        {
+            if (error == null)
+                return $"Successfully disabled all {debugTriggerCount} debug triggers.";
+
+            return error;
+        }
+
+        private string error;
+
+        private const string debugString = "debug";
+        private int debugTriggerCount;
+
+        /// <summary>
+        /// The function that actually does the magic.
+        /// </summary>
+        /// <param name="map">Map argument that allows us to access map data.</param>
+        public void Perform(Map map)
+        {
+            var debugTriggers = map.Triggers.FindAll(trigger => trigger.Name.Contains(debugString, StringComparison.CurrentCultureIgnoreCase));
+            if (debugTriggers.Count == 0)
+            {
+                error = "No debug triggers found!";
+                return;
+            }
+
+            foreach (var debugTrigger in debugTriggers)
+            {
+                debugTrigger.Disabled = true;                
+            }
+
+            debugTriggerCount = debugTriggers.Count;
+        }
+    }
+}
