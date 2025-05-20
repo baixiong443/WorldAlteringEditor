7b2b37db0afcd0f383367450de61bb63a993dd32
Rampastring <rampastring@outlook.com>
2025-05-20 21:00:52 +0300

    Fix extra light not getting applied to remap

--- src/TSMapEditor/Rendering/ObjectRenderers/ObjectRenderer.cs
+++ src/TSMapEditor/Rendering/ObjectRenderers/ObjectRenderer.cs
@@ -420,12 +420,12 @@ namespace TSMapEditor.Rendering.ObjectRenderers
                 if (affectedByLighting && image.SubjectToLighting)
                 {
                     lighting = mapCell.CellLighting.ToXNAVector4(extraLight);
-                    remapColor = ScaleColorToAmbient(remapColor, mapCell.CellLighting);
+                    remapColor = ScaleColorToAmbient(remapColor, lighting);
                 }
                 else if (affectedByAmbient)
                 {
                     lighting = mapCell.CellLighting.ToXNAVector4Ambient(extraLight);
-                    remapColor = ScaleColorToAmbient(remapColor, mapCell.CellLighting);
+                    remapColor = ScaleColorToAmbient(remapColor, lighting);
                 }
             }
 
@@ -473,7 +473,7 @@ namespace TSMapEditor.Rendering.ObjectRenderers
             depthAddition += GetDepthFromPosition(gameObject, drawingBounds);
             depthAddition += GetDepthAddition(gameObject);
 
-            remapColor = ScaleColorToAmbient(remapColor, mapCell.CellLighting);
+            remapColor = ScaleColorToAmbient(remapColor, lighting);
 
             // Shader scales lighting by 2x
             remapColor = new Color(remapColor.R / 2, remapColor.G / 2, remapColor.B / 2, remapColor.A);
@@ -530,6 +530,16 @@ namespace TSMapEditor.Rendering.ObjectRenderers
         protected Color ScaleColorToAmbient(Color color, MapColor mapColor)
         {
             double highestComponent = Math.Max(mapColor.R, Math.Max(mapColor.G, mapColor.B));
+
+            return new Color((int)(color.R * highestComponent),
+                (int)(color.G * highestComponent),
+                (int)(color.B * highestComponent),
+                color.A);
+        }
+
+        protected Color ScaleColorToAmbient(Color color, Vector4 vector)
+        {
+            double highestComponent = Math.Max(vector.X, Math.Max(vector.Y, vector.Z));
 
             return new Color((int)(color.R * highestComponent),
                 (int)(color.G * highestComponent),
