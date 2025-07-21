00837f321d7e093cacacaf4b72466b8a661d1726
Rampastring <rampastring@outlook.com>
2025-07-21 13:24:43 +0300

    Reset right-click scrolling position when MapUI isn't the active control

--- src/TSMapEditor/UI/MapUI.cs
+++ src/TSMapEditor/UI/MapUI.cs
@@ -548,14 +548,24 @@ namespace TSMapEditor.UI
         {
             inputEventArgs.Handled = true;
 
-            if (CursorAction != null && !isRightClickScrolling)
+            if (isRightClickScrolling)
+            {
+                StopRightClickScrolling();
+            }
+            else if (CursorAction != null)
             {
                 CursorAction = null;
             }
 
-            isRightClickScrolling = false;
+            StopRightClickScrolling();
 
             base.OnRightClick(inputEventArgs);
+        }
+
+        private void StopRightClickScrolling()
+        {
+            isRightClickScrolling = false;
+            rightClickScrollInitPos = new Point(-1, -1);
         }
 
         private MapTile CalculateBestTileUnderCursor()
@@ -612,6 +622,10 @@ namespace TSMapEditor.UI
                             Camera.FloatTopLeftPoint.Y + result.Y * rightClickScrollRate);
                     }
                 }
+            }
+            else if (isRightClickScrolling)
+            {
+                StopRightClickScrolling();
             }
 
             if (leftPressedDownOnControl && !Cursor.LeftDown)
