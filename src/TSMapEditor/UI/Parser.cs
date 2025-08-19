838d2e2d35b70e5c23c06c368cc759a67ffb1570
Rampastring <rampastring@outlook.com>
2025-08-20 01:20:47 +0300

    Add parser constants for cell width and height

--- src/TSMapEditor/UI/Parser.cs
+++ src/TSMapEditor/UI/Parser.cs
@@ -29,6 +29,8 @@ namespace TSMapEditor.UI
             globalConstants.Add("IS_RA2YR", Constants.IsRA2YR ? 1 : 0);
             globalConstants.Add("OBJECT_HEALTH_MAX", Constants.ObjectHealthMax);
             globalConstants.Add("IS_FLAT_WORLD", Constants.IsFlatWorld ? 1 : 0);
+            globalConstants.Add("CELL_WIDTH", Constants.CellSizeX);
+            globalConstants.Add("CELL_HEIGHT", Constants.CellSizeY);
 
             _instance = this;
         }
